31aa7a40fe10f1eeeaf8ad832b0be6cdde4c6cc9
Fun <161741261+funfunido@users.noreply.github.com>
2025-12-02 10:17:48 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let senderID = 2; 
+radio.setGroup(1);
+
+
+input.onButtonPressed(Button.A, function () {
+
+})
+
+input.onButtonPressed(Button.B, function () {
+
+})
+
+input.onPinPressed(TouchPin.P1, function() {
+    
+})
