e9707daaf5a1b2f4cefad72619c5e12582703bef
Github-coder-plus <181423861+Github-coder-plus@users.noreply.github.com>
2025-12-02 10:20:48 -0700

    Add senderID and configure radio group
    Added senderID variable and set radio group.

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+// Base code made by Git-coder-plus
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
+    basic.pause(200)
+    basic.clearScreen()
+})
+radio.setGroup(1)
+radio.setTransmitPower(1)
+let id = 1
+basic.showNumber(id)
+basic.pause(2000)
+basic.clearScreen()
+basic.forever(function () {
+    radio.sendNumber(id)
+    basic.pause(200)
+})
+
+
+
+
+
 let senderID = 2; 
 radio.setGroup(1);
 
