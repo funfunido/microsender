7a9738a44c2f3a6f9f8854d74b8ccd717d3a078e
Github-coder-plus <181423861+Github-coder-plus@users.noreply.github.com>
2025-12-02 10:31:30 -0700

    added code comments

--- main.ts
+++ main.ts
@@ -1,21 +1,31 @@
-//  Base code made by Git-coder-plus
-radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
-    basic.showNumber(receivedNumber)
-    basic.pause(200)
-    basic.clearScreen()
+// Base code made by Git-coder-plus
+
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)  // when a number ID is sent it will detect the number that was sent to it and display it.
+    basic.pause(200) // pauses so you see the numebr for some time
+    basic.clearScreen() // clears screen (ofc)
 })
-radio.setGroup(1)
-radio.setTransmitPower(1)
-let clientId = 1
-basic.showNumber(clientId)
-basic.pause(2000)
-basic.clearScreen()
-basic.forever(function on_forever() {
-    radio.sendNumber(clientId)
-    basic.pause(200)
+
+radio.setGroup(1) // this is our radio group
+radio.setTransmitPower(1) // we should make a setting to change the transmit power
+
+let id = 1 // when we upload this code to the microbit use a unique ID
+
+basic.showNumber(id) // shows your id at the begenning
+basic.pause(2000) // pauses for 2 secs
+basic.clearScreen() // clears screen
+
+basic.forever(function () {
+    radio.sendNumber(id) // constnatly sends id
+    basic.pause(200) // pauses
 })
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+
+// end of base code made by me git-coder plus
+let senderID = 2; 
+radio.setGroup(1);
+
+input.onButtonPressed(Button.A, function () {
+
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
@@ -23,3 +33,5 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
 input.onPinPressed(TouchPin.P1, function on_pin_pressed_p1() {
     
 })
+
+
